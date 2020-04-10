fe48006a81c38b20fd5daa596e61b861be85c7aa
boutique-lafolie <mehdiayad@hotmail.fr>
2020-04-10 06:05:53 +0000

    updates

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -57,6 +57,15 @@
                                 </a>
 
                                 <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    
+                                 	<a class="dropdown-item" href="{{ route('telescope') }}">
+                                        Monitoring
+                                    </a> 
+                                    
+                                    <a class="dropdown-item d-none">
+                                         Environnement : {{ config('app.env') }}
+                                    </a> 
+                                    
                                     <a class="dropdown-item" href="{{ route('logout') }}"
                                        onclick="event.preventDefault();
                                                      document.getElementById('logout-form').submit();">
@@ -66,14 +75,6 @@
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
                                     </form>
-                                    
-                                 	<a class="dropdown-item" href="{{ route('telescope') }}">
-                                        Monitoring
-                                    </a> 
-                                    
-                                    <a class="dropdown-item">
-                                         Environnement : {{ config('app.env') }}
-                                    </a> 
                                                                         
                                 </div>
                                 
