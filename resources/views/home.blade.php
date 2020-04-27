6d9322ff8d8204a62760427a605009ed9dd1565c
boutique-lafolie <mehdiayad@hotmail.fr>
2020-04-27 20:49:43 +0000

    updates

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -7,7 +7,7 @@
             <div class="card">
                 <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
+                <div class="card-body bg-info text-white">
                    
                     @if (session('status'))
                         <div class="alert alert-success" role="alert">
